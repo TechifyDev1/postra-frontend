ea003334c978b6ea53c26e5c4a64430191a4d210
techifydev <yusufabdulqudus02@gmail.com>
2026-02-07 18:02:08 +0100

    Updated backend base url

--- utils.ts
+++ utils.ts
@@ -1,4 +1,4 @@
-const baseurl = "http://localhost:8080/api";
+const baseurl = "https://postra-backend.onrender.com/api";
 export const createUserUrl = `${baseurl}/users/register`;
 export const loginUrl = `${baseurl}/users/login`;
 export const getUserUrl = (username: string): string => `${baseurl}/users/profile/${username}`;
