b4114a73bc1299b789d2c3b190e18d0ae18d1d25
techifydev <yusufabdulqudus02@gmail.com>
2026-02-09 14:18:37 +0100

    Making user of user's image everywhere

--- components/main-components/cell/image-avatar/ImageAvatar.tsx
+++ components/main-components/cell/image-avatar/ImageAvatar.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import { ImageAvatarProps } from '@/types/types';
 import style from './ImageAvatar.module.css';
 import Image from "next/image";
 import Link from 'next/link';
+import { useUserContext } from '@/hooks/use-user-context';
 
 const ImageAvatar = ({ src, alt, size = 'medium', username }: ImageAvatarProps) => {
+    const { user } = useUserContext();
     return (
         <Link className={`${style.ImageAvatar} ${style[size]}`} href={`/${username}`}>
             <Image
-                src={src}
+                src={user?.profilePictureUrl ?? src}
                 alt={alt ?? 'Avatar'}
                 layout="fill"
                 style={{ objectFit: 'cover' }}
